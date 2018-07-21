d496aa6d1b5a9261e2aa10c8bc619666f416682d
Raul Prado <raul.prado002@mymdc.net>
2018-07-21 17:29:17 +0000

    update

---
+++ src/js/component/userPage/userNavbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class UserNavbar extends React.Component{
+    render(){
+        return(
+            <nav class="navbar navbar-dark bg-dark">
+                <span class="navbar-brand mb-0 h1">Navbar</span>
+            </nav>
+        );
+    }
+}

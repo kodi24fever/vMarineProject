d0ec4a9d48bb0b083e9d0a9c87d278d0378f69fe
Frank Izquierdo <frankdavid93om@yahoo.com>
2018-07-21 16:27:45 +0000

    some commit

--- src/js/component/login.jsx
+++ src/js/component/login.jsx
@@ -10,21 +10,6 @@ export default class Login extends Component {
       password: ""
     };
   }
-  /*
-  validateForm() {
-    return this.state.email.length > 0 && this.state.password.length > 0;
-  }
-
-  handleChange = event => {
-    this.setState({
-      [event.target.id]: event.target.value
-    });
-  }
-
-  handleSubmit = event => {
-    event.preventDefault();
-  }
-  */
   
   render() {
     return (
